0d556ff7d518cb4a1c4b45bbded4a66cf0fc0227
Nishtha Chopra <nishthac3@gmail.com>
2023-04-07 20:17:48 +0530

    Update Challenge43.java

--- Challenge43.java
+++ Challenge43.java
@@ -28,25 +28,32 @@ Sample Output 0:
 
 import java.util.Scanner;
 
-interface Test {
+interface Test 
+{
     int count(int[] arr);
 }
 
-public class Demo {
-    public static void main(String[] args) {
+public class Demo 
+{
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             arr[i] = sc.nextInt();
         }
 
         // Lambda expression to count prime numbers in the array
-        Test primeCount = (int[] a) -> {
+        Test primeCount = (int[] a) -> 
+        {
             int count = 0;
-            for (int num : a) {
-                if (isPrime(num)) {
+            for (int num : a) 
+            {
+                if (isPrime(num)) 
+                {
                     count++;
                 }
             }
@@ -54,10 +61,13 @@ public class Demo {
         };
 
         // Lambda expression to count even numbers divisible by 5 in the array
-        Test evenDivBy5Count = (int[] a) -> {
+        Test evenDivBy5Count = (int[] a) -> 
+        {
             int count = 0;
-            for (int num : a) {
-                if (num % 2 == 0 && num % 5 == 0) {
+            for (int num : a) 
+            {
+                if (num % 2 == 0 && num % 5 == 0) 
+                {
                     count++;
                 }
             }
@@ -68,15 +78,141 @@ public class Demo {
     }
 
     // Utility method to check if a number is prime
-    private static boolean isPrime(int num) {
-        if (num < 2) {
+    private static boolean isPrime(int num) 
+    {
+        if (num < 2) 
+        {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
+        for (int i = 2; i <= Math.sqrt(num); i++) 
+        {
+            if (num % i == 0) 
+            {
                 return false;
             }
         }
         return true;
+    }
+}
+
+/*
+LPU is going to host the Day 1 placement drive for the 4th year students of B.Tech. program on 25th August 2023 in which 
+3 MNCs are going to counduct the recrutiment for all the students who are eligible for the placement based on the minimum CGPA criteria.
+
+All the 3 companies are having their own minimum CGPA requirement and they wanted to implement a JAVA PROGRAM 
+which can give the count of all the eligible students for each company.
+
+The Program must read the number of students N and their CGPA and store all the N student objects in an array. 
+Invoke the filterStudents(Student [] cgpa, CompanyNorms ob) method by passing the Lambda expressions corresponding to 
+the minimum cgpa norms of the company and print the count of the students.
+
+Input Format:
+First Line reads the number of students N
+
+Second Line reads the cgpa of N students separated by SPACE
+
+Third Line reads the minimum cgpa requirement of 3 companies separated by SPACE
+
+Constraints:
+g
+
+Output Format:
+Prints 3 numbers separated by SPACE i.e. number of eligible students for the 3 companies respectively
+
+Sample Input 0:
+2
+7.19 2.5
+5.5 7.0 6.5
+
+Sample Output 0:
+1 1 1 
+
+Sample Input 1:
+3
+4.25 6.08 5.19
+7.5 6.0 5.5
+
+Sample Output 1:
+0 1 1 
+*/
+
+
+import java.util.Scanner;
+
+interface CompanyNorms 
+{
+    boolean isEligible(Student s);
+}
+
+class Student 
+{
+    private double cgpa;
+
+    public Student(double cgpa) 
+    {
+        this.cgpa = cgpa;
+    }
+
+    public double getCgpa() 
+    {
+        return cgpa;
+    }
+}
+
+public class PlacementDrive 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+
+        // Reading the CGPA of N students
+        for (int i = 0; i < n; i++) 
+        {
+            double cgpa = sc.nextDouble();
+            students[i] = new Student(cgpa);
+        }
+
+        // Reading the minimum CGPA requirement of the 3 companies
+        double[] companyNorms = new double[3];
+        for (int i = 0; i < 3; i++) 
+        {
+            companyNorms[i] = sc.nextDouble();
+        }
+
+        // Lambda expressions to check if a student is eligible for the company
+        CompanyNorms[] companyFilters = new CompanyNorms[3];
+        companyFilters[0] = (Student s) -> s.getCgpa() >= companyNorms[0];
+        companyFilters[1] = (Student s) -> s.getCgpa() >= companyNorms[1];
+        companyFilters[2] = (Student s) -> s.getCgpa() >= companyNorms[2];
+
+        // Counting the number of eligible students for each company
+        int[] eligibleCounts = new int[3];
+        for (int i = 0; i < 3; i++) 
+        {
+            eligibleCounts[i] = filterStudents(students, companyFilters[i]);
+        }
+
+        // Printing the eligible counts
+        for (int count : eligibleCounts) 
+        {
+            System.out.print(count + " ");
+        }
+    }
+
+    // Method to filter the students who are eligible for a given company
+    private static int filterStudents(Student[] students, CompanyNorms filter) 
+    {
+        int count = 0;
+        for (Student s : students) 
+        {
+            if (filter.isEligible(s)) 
+            {
+                count++;
+            }
+        }
+        return count;
     }
 }
