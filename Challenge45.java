b63c588df881014c2dca103da3d28873443a3a64
Nishtha Chopra <nishthac3@gmail.com>
2023-04-10 22:47:21 +0530

    Create Challenge45.java

---
+++ Challenge45.java
@@ -0,0 +1,33 @@
+/*
+Hemant is Store owner. In order to increase his sale he imposed a restriction on buying the trousers but there is a restriction 
+that the customer cant buy trouser alone, the customer has to purchase shirt first then only he can buy trousers but 
+single shirt can be buyied individually.Develop a program in order to increase his sale.
+
+Input Format:
+First line will contain one integer value N representing number of items purchased.
+Next N lines will contain one integer value 0 or 1 representing sequence of items purchased(0 means Shirt and 1 means Trouser).
+
+Constraints:
+N can't be negative.
+Trouser cant be buyed without shirt. After fisrt line in the input the value should either be 0 or 1, otherwise output will be printed Invalid input
+
+Output Format:
+Valid Purchase/Invalid Purchase
+
+Sample Input 0:
+3
+1
+1
+0
+
+Sample Output 0:
+Invalid Purchase
+
+Sample Input 1:
+2
+2
+3
+
+Sample Output 1:
+Invalid input
+*/
