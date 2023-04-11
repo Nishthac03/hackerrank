b20d20778345961b33e0a10401f71192d58bffbe
Nishtha Chopra <nishthac3@gmail.com>
2023-04-11 23:26:13 +0530

    Create Challenge46.java

---
+++ Challenge46.java
@@ -0,0 +1,34 @@
+/*
+
+Cube and cuboid have a difference that in cube all the six faces are square while in cuboid all faces are rectangular.
+Write a program in which user will enter length, breath, and height and identify that wheather it is a cuboid or not. 
+If user input is same for length, breath, and height then it will be cube and program should through InvalidCuboidException. 
+Do the exception handling and print Invalid Cuboid.
+
+Input Format:
+Three space seperated integers representing length, breath, and height.
+
+Constraints:
+Maximum value can be 10.
+
+Output Format:
+Valid Cuboid/ Invalid Cuboid/ Invalid Input
+
+Sample Input 0:
+5 10 5
+
+Sample Output 0:
+Valid Cuboid
+
+Sample Input 1:
+5 7 12
+
+Sample Output 1:
+Invalid Input
+
+Sample Input 2:
+4 4 4
+
+Sample Output 2:
+Invalid Cuboid
+*/
