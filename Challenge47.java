9b23c846e514b7e098aeb42f9c39d88858a8436d
Nishtha Chopra <nishthac3@gmail.com>
2023-04-12 20:58:19 +0530

    Update Challenge47.java

--- Challenge47.java
+++ Challenge47.java
@@ -70,3 +70,37 @@ public class Solution
         sc.close();
     }
 }
+
+
+/*
+Hari ask Rama to make an arithmatic calculator which will accpet two integer values. but at a time only one opatation(+,-,*,/) he can do.
+Help rama to create this using labda expression.
+
+Input Format:
+First line will contain two ineteger values on which we have to perform the operation
+Second line will contain one integer value which will describe the operation
+1 addition
+2 subtraction
+3 multiplication
+4 division
+
+Constraints:
+number should be integer value
+
+Output Format:
+one integer value represnting result of the operation if wrong input then print Invalid
+
+Sample Input 0:
+7 8
+2
+
+Sample Output 0:
+-1
+
+Sample Input 1:
+4 5
+5
+
+Sample Output 1:
+Invalid
+*/
