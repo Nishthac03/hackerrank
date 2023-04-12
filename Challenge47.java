921127b3fdd03fcf03514f57c717b92b15e6552a
Nishtha Chopra <nishthac3@gmail.com>
2023-04-12 20:44:57 +0530

    Create Challenge47.java

---
+++ Challenge47.java
@@ -0,0 +1,33 @@
+/*
+Shubham is in the Shopping mall and he wants to buy trouser. The sales man asks for the waist size from him. 
+The size of trousers available in the mall is between 18 inches to 40 inches. If the customer asks for 
+trouser size less than 18 and greater than 40 then an UnexpectedTrouserSizeException is thrown. 
+Handle the exception and print the demanded size is out of stock
+
+Input Format:
+One integer value representing waist of the trouser.
+
+Constraints:
+Size should be in integer only.
+
+Output Format:
+try and shop/the demanded size is out of stock/invalid input
+
+Sample Input 0:
+20
+
+Sample Output 0:
+try and shop
+
+Sample Input 1:
+17
+
+Sample Output 1:
+the demanded size is out of stock
+
+Sample Input 2:
+16.6
+
+Sample Output 2:
+invalid input
+*/
